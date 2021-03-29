ea25070a3530030589ae3c6b3d0c135d49dd509b
Training <Training@PC>
2021-03-29 16:47:14 -0600

    add: botton in menu

--- src/test/java/Runner/GUI.java
+++ src/test/java/Runner/GUI.java
@@ -3,6 +3,7 @@ package Runner;
 import excelManager.ReadExcelFile;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -10,86 +11,150 @@ import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.io.IOException;
 
-public class GUI {
+public class GUI extends JFrame{
+	 JScrollPane scrollpane;
+	 JRadioButton[] radio ;
+	  String path = "";
+     String fileName = "";
+     
+	  public GUI() {
+		    super("Select Test Cases");
+		    setSize(600, 400);
+		    setDefaultCloseOperation(EXIT_ON_CLOSE);
+		    init();
+		  }
+	  
+	  public void init() {
+		  
+		     javax.swing.JFileChooser jfc = new javax.swing.JFileChooser();
+		        FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel files", "xls", "xlsx");
+		        jfc.setFileFilter(filter);
+		      
+		     
+		        try {
+		            if(jfc.showSaveDialog(null) == jfc.APPROVE_OPTION){
+		                path = jfc.getSelectedFile().getPath();
+		                fileName = jfc.getSelectedFile().getName();
+		            }
+		        } catch (Exception e){
+		            e.printStackTrace();
+		        }
 
-    public String[] showGui() throws IOException {
-        javax.swing.JFileChooser jfc = new javax.swing.JFileChooser();
-        String path = "";
-        String fileName = "";
-        String[] tcSelected;
-        String[] dataReturn = new String[3];
-        try {
-            if(jfc.showSaveDialog(null) == jfc.APPROVE_OPTION){
-                path = jfc.getSelectedFile().getPath();
-                fileName = jfc.getSelectedFile().getName();
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        ReadExcelFile excel = new ReadExcelFile();
-
-        //Window to select test case
-
-        /** The array namesTc save to names the Test cases */
-
-        Object[] objArr = excel.getSheets(path).toArray();
-        String[] namesTc = new String[objArr.length];
-        System.arraycopy(objArr, 0, namesTc, 0, objArr.length);
-
-        JRadioButton[] radio = new JRadioButton[namesTc.length];
-        //create panel interface
-        JPanel gui = new JPanel(new BorderLayout(1,1));
-        JPanel panel = new JPanel(new GridLayout(0,1));
-        JScrollPane scroll = new JScrollPane(panel);
-        //scroll.setBounds(5,10,140,100);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scroll.setPreferredSize(new Dimension(100,200));
-        gui.add(scroll);
-        
-        JLabel label = new JLabel();
-        label.setText("Which Test Cases wish execute");
-        gui.add(label, BorderLayout.NORTH);
-        /** This for create and add to array radio all the buttons that we will show */
-        String name = "";
-        for (int u = 0; u < namesTc.length; u++) {
-
-            name = namesTc[u];
-            if (name != null) {
-                JRadioButton button = new JRadioButton(name);
-                radio[u] = button;
-                panel.add(radio[u]);
-            }
-
-            if(radio[u] == null) {
-                panel.remove(radio[u]);
-            }
-
-        }
-
-        
-        /** Show the Panel */
-        JOptionPane.showMessageDialog(null, gui);       
-        
-        /** This for save the selected Test cases */
-        tcSelected = new String[radio.length];
-        String radios="";
-        for (int t = 0; t < radio.length; t++) {
-
-            if (radio[t].isSelected()) {
-
-            	radios += radio[t].getText()+"-";
-
-            }
-        }
-
-        dataReturn[0] = path;
-        dataReturn[1] = fileName;
-        dataReturn[2] = radios;
-
-        return dataReturn;
+		        
+		     
+		        String[] namesTc = {};
+		        ReadExcelFile excel = new ReadExcelFile();
+		        try {
+		        	 Object[] objArr = excel.getSheets(path).toArray();
+				      
+				       namesTc= new String[objArr.length];
+				        System.arraycopy(objArr, 0, namesTc, 0, objArr.length);
+		        } catch (IOException e){
+		            e.printStackTrace();
+		        }
 
 
-    }
+		         radio = new JRadioButton[namesTc.length];
+		     
+		        //create panel interface
+
+		     
+		     JButton btnAll = new javax.swing.JButton();
+		     btnAll.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+		     btnAll.setForeground(new java.awt.Color(51, 51, 255));
+		     btnAll.setText("Select all TCs");
+		     btnAll.setFocusable(false);
+		     btnAll.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+		     btnAll.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
+		     btnAll.addActionListener(new java.awt.event.ActionListener() {
+		            public void actionPerformed(java.awt.event.ActionEvent evt) {
+		                btnAllActionPerformed(evt);
+		            }
+		        });
+		     JButton btnClear = new javax.swing.JButton();
+		     btnClear.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+		     btnClear.setForeground(new java.awt.Color(51, 51, 255));
+		     btnClear.setText("Clear Selection");
+		     btnClear.setFocusable(false);
+		     btnClear.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+		     btnClear.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+		     btnClear.addActionListener(new java.awt.event.ActionListener() {
+		            public void actionPerformed(java.awt.event.ActionEvent evt) {
+		                btnClearActionPerformed(evt);
+		            }
+		        });	       
+		 
+		   JPanel p = new JPanel();
+		//   JPanel p2 = new JPanel(new BorderLayout(1,5));
+		  JPanel p2 = new JPanel();
+		    p.setSize(600, 400);
+		    p.setLayout(new GridLayout(4, 4));
+		    
+			   p.add(btnAll);
+			   p.add(btnClear);
+		   p2.add(p);
+		   
+	       String name = "";
+	        for (int u = 0; u < namesTc.length; u++) {
+
+	            name = namesTc[u];
+	            if (name != null) {
+	                JRadioButton button = new JRadioButton(name);
+	                radio[u] = button;
+	                p.add(radio[u]);
+	            
+	            }
+
+	            if(radio[u] == null) {
+	                p.remove(radio[u]);
+	            }
+
+	        }
+
+		   
+		    scrollpane = new JScrollPane(p2);
+		    scrollpane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		    getContentPane().add(scrollpane, BorderLayout.CENTER);
+		  }
+	  
+		  private void btnAllActionPerformed(java.awt.event.ActionEvent evt) {
+			 
+			  for (int t = 0; t < radio.length; t++) {
+		           radio[t].setSelected(true);		            
+		        }
+		  }
+		    
+		  private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {
+				 
+			  for (int t = 0; t < radio.length; t++) {
+		           radio[t].setSelected(false);		            
+		        }
+		  }	
+
+   public String[] showGui() throws IOException {
+   	// String[] tcSelected;
+   	  String[] dataReturn = new String[4];
+       
+       /** Show the Panel */
+      JOptionPane.showMessageDialog(null, scrollpane);
+
+       /** This for save the selected Test cases */
+    //  tcSelected = new String[radio.length];
+       String radios="";
+       for (int t = 0; t < radio.length; t++) {
+
+           if (radio[t].isSelected()) {
+               radios += radio[t].getText()+"-";
+
+           }
+       }
+
+       dataReturn[0] = path;
+       dataReturn[1] = fileName;
+       dataReturn[2] = radios;
+
+      return dataReturn;
+
+   }
 
 }
